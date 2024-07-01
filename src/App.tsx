7ad15e9683974843a38c9b48ef67df58c3770a16
syk25 <syk2245@gmail.com>
2024-07-02 01:09:50 +0900

    mediapipe

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import './App.css';
 import { Canvas, useGraph } from '@react-three/fiber';
 import { Color } from 'three';
 import { useGLTF } from '@react-three/drei';
+import { FaceLandmarker, FilesetResolver } from '@mediapipe/tasks-vision';
 
 let video: HTMLVideoElement;
+let faceLandmarker: FaceLandmarker;
+let lastVideoTime = -1;
 
 function App() {
   /*  */
@@ -15,13 +18,40 @@ function App() {
 
 
   /* 비디오 설정 */
-  const setup = () => {
+  const setup = async () => {
+    const vision = await FilesetResolver.forVisionTasks(
+
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    );
+    faceLandmarker = await FaceLandmarker.createFromOptions(
+      vision,
+      {
+        baseOptions: {
+          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+          delegate: "GPU"
+        },
+        outputFaceBlendshapes: true,
+        outputFacialTransformationMatrixes: true,
+        runningMode: "VIDEO"
+      });
     video = document.getElementById('video') as HTMLVideoElement;
     navigator.mediaDevices.getUserMedia({
       video: { width: 1280, height: 720 }
     }).then((stream) => {
       video.srcObject = stream;
+      video.addEventListener('loadeddata', predict)
     })
+  }
+
+  const predict = () => {
+    let startTimeMs = performance.now();
+    if (lastVideoTime !== video.currentTime) {
+      lastVideoTime = video.currentTime;
+      const result = faceLandmarker.detectForVideo(video, startTimeMs)
+      console.log(result)
+    }
+
+    requestAnimationFrame(predict);
   }
 
   /*  */
@@ -38,9 +68,9 @@ function App() {
         backgroundColor: 'purple',
         height: 400,
       }}
-      camera={{
-        fov: 25
-      }}>
+        camera={{
+          fov: 25
+        }}>
 
         <ambientLight intensity={0.5} />
         <pointLight position={[1, 1, 1]} color={new Color(1, 0, 0)} intensity={0.5} />
